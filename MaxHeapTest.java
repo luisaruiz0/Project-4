ba6eb84054723ed1aad49fb575d620eb403b6a29
luisaruiz0 <luis.a.r.0212@gmail.com>
2022-04-19 21:15:37 -0700

    FileWriter output.txt temp

--- MaxHeapTest.java
+++ MaxHeapTest.java
@@ -1,3 +1,30 @@
-public class MaxHeapTest {
-    
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class MaxHeapTest 
+{
+    public static void main(String[] args) throws IOException
+    {
+        FileWriter file1 = new FileWriter("output.txt");
+        file1.write("Heap built using sequential insertions: ");
+        // output of first 10 ints of array here
+        file1.write("Number of swaps in the heap creation: ");
+        // output of number of swaps here
+        file1.write("Heap after 10 removals:  \n");
+        // output of heaps after 10 removals
+       
+        
+        /*
+            optimal method
+        */
+        file1.write("\nHeap built using optimal insertions: ");
+        // output of first 10 ints of array here
+        file1.write("Number of swaps in the heap creation: ");
+        // output of number of swaps here
+        file1.write("Heap after 10 removals:  ");
+        // output of heaps after 10 removals
+        file1.close();
+
+    }
 }
